519308b70f8e8e1179c3a460fe9dd271772b387b
natf17 <natfar.dev@gmail.com>
2019-09-26 14:02:52 -0400

    Beginning tests

---
+++ src/test/java/com/ppublica/shopify/security/configurer/ShopifySecurityConfigurerTest.java
@@ -0,0 +1,8 @@
+package com.ppublica.shopify.security.configurer;
+
+import org.junit.Rule;
+
+public class ShopifySecurityConfigurerTest {
+	@Rule
+	public final SpringTestRule spring = new SpringTestRule();
+}
